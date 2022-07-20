c48a7e5411c38f03754ba4f4f44e6a4c5e64dab2
Guillaume <miche.guillaume@isitparis.eu>
2022-07-20 17:26:43 +0200

    init ball bridge

---
+++ my-first-game/Assets/DodgeBallBehaviour3.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeBallBehaviour3 : MonoBehaviour
+{
+    Vector3 pointA = new Vector3(-6, 3, -28);
+    Vector3 pointB = new Vector3(6, 3, -28);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+    }
+}
